9e77f54d44cf9cd7f39ed3752d87886aa2a2b185
SameerShanbhogue <84760553+SameerShanbhogue@users.noreply.github.com>
2024-02-14 16:18:24 +0530

    Update train.py

--- train.py
+++ train.py
@@ -235,7 +235,7 @@ def train_model(config):
             label = batch['label'].to(device)
 
             loss = loss_fn(proj_output.view(-1,tokenizer_tgt.get_vocab_size()),label.view(-1))
-            batch_iterator.set_postfix({"loss":f"(loss.item(:6.3f))"})
+            batch_iterator.set_postfix({"loss":f"{loss.item():6.3f}"})
 
             writer.add_scalar('train loss', loss.item(),global_step)
             writer.flush()
